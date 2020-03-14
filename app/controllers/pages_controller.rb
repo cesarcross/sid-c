31b06c983ea5ea8498b0f4d9c5a7abc7eb14e494
Vinicius Alvim <vini0191@gmail.com>
2020-03-14 19:22:12 -0300

    ongoing...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,21 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def sobre
-  end
-
-  def produtos
-  end
-
   def contato
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.deliver
+      flash[:notice] = 'Mensagem enviada com sucesso! Responderemos em breve.'
+      redirect_to contato_path
+    else
+      render :contato
+    end
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:name, :email, :phone, :message)
   end
 end
---
+++ app/models/page.rb
@@ -0,0 +1,20 @@
+class Page < MailForm::Base
+  attribute :name, validate: true
+  attribute :email, validate: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  attribute :phone, validate: true
+  validates :phone, numericality: true
+  attribute :message, validate: true
+
+  def headers
+    {
+      # this is the subject for the email generated, it can be anything you want
+      # subject: "My Contact Form",
+      subject: "Formulário",
+      # to: 'your.email@yourdomain.com',
+      to: 'igor@sigdiamantados.com.br',
+      from: %("#{name}" <#{email}>)
+      # the from will display the name entered by the user followed by the email
+    }
+  end
+end
